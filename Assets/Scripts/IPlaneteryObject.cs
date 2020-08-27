af2faf0c91f173c8400dedee456d84082259e395
Ashypilo <shipilov2332@gmail.com>
2020-08-27 20:18:08 +0300

    Planetary

---
+++ Assets/Scripts/IPlaneteryObject.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MassClass
+{
+    Asteroidan,
+    Mercurian,
+    Subterran,
+    Terran,
+    Superterran,
+    Neptunian,
+    Jovian
+}
+
+public class IPlaneteryObject
+{
+
+    private MassClass massClass;
+
+    private float mass;
+    private float radius;
+    private int rand;
+
+    public void init() {
+
+        rand = Random.Range(0, 6);
+        switch (rand) {
+            case 0:
+                massClass = MassClass.Asteroidan;
+                mass = Random.Range(0f, 0.00001f);
+                radius = Random.Range(0f, 0.03f);
+                break;
+            case 1:
+                massClass = MassClass.Mercurian;
+                mass = Random.Range(0.00001f, 0.1f);
+                radius = Random.Range(0.03f, 0.7f);
+                break;
+            case 2:
+                massClass = MassClass.Subterran;
+                mass = Random.Range(0.1f, 0.5f);
+                radius = Random.Range(0.5f, 1.2f);
+                break;
+            case 3:
+                massClass = MassClass.Terran;
+                mass = Random.Range(0.5f, 2f);
+                radius = Random.Range(0.8f, 1.9f);
+                break;
+            case 4:
+                massClass = MassClass.Superterran;
+                mass = Random.Range(2f, 10f);
+                radius = Random.Range(1.3f, 3.3f);
+                break;
+            case 5:
+                massClass = MassClass.Neptunian;
+                mass = Random.Range(10f, 50f);
+                radius = Random.Range(2.1f, 5.7f);
+                break;
+            case 6:
+                massClass = MassClass.Jovian;
+                mass = Random.Range(50f, 5000f);
+                radius = Random.Range(3.5f, 27f);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public MassClass getClass() {
+        return massClass;
+    }
+
+    public float getMass() {
+        return mass;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+}
---
+++ Assets/Scripts/Objects.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Objects : MonoBehaviour
+{
+    private GameObject[] sphere;
+    private IPlaneteryObject[] planet;
+
+    public int numRand;
+    public float angle = 1;
+    public float radius = 0.5f;
+    public float speed = 0.5f;
+
+    void Start()
+    {
+        if (numRand == 0)
+            numRand = Random.Range(1, 30);
+        sphere = new GameObject[numRand];
+        planet = new IPlaneteryObject[numRand];
+
+        for (int i = 0; i < numRand; i++) {
+            IPlaneteryObject planCopy = new IPlaneteryObject();
+            planCopy.init();
+            planet[i] = planCopy;
+            Debug.Log(planet[i].getClass());
+        }
+
+        for (int i= 0; i < numRand; i++) {
+            GameObject sphereCop = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere[i] = sphereCop;
+        }
+    }
+
+    void Update()
+    {
+        angle += Time.deltaTime;
+
+        float space = 1;
+        for (int i = 0; i < numRand; i++) {
+        var x = Mathf.Cos ((angle * speed)/ planet[i].getMass());
+        var y = Mathf.Sin ((angle * speed)/ planet[i].getMass());
+            sphere[i].transform.localScale = new Vector3(planet[i].getRadius(), planet[i].getRadius(), planet[i].getRadius());
+            space = space + 10;
+
+            sphere[i].transform.position = new Vector2(x * space , y * space);
+        }
+    }
+}
+
+
